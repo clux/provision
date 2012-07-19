ab07acc6a4b8fd1ef91ffb541025abbaad23f646
clux <analsandblaster@gmail.com>
2012-07-19 21:21:48 +0100

    git script sets up config and as much of the keys as possible too

--- git.sh
+++ git.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# set name and email
+git config --global user.name "clux"
+git config --global user.email "analsandblaster@gmail.com"
+
 # pretty log from http://coderwall.com/p/euwpig?i=3&p=1&t=git
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
@@ -12,3 +16,8 @@ git config --global alias.st "status -s"
 # short commit alias
 git config --global alias.ci "commit"
 git config --global alias.cia "commit -am"
+
+# generate ssh keys
+ssh-keygen -t rsa -C "analsandblaster@gmail.com"
+echo "LOG IN TO GITHUB AND PASTE THE FOLLOWING YOUR SSH KEYS SECTION:"
+cat ~/.ssh/id_rsa.pub
