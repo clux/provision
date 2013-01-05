756f7ffdb4cd70bde3f61f33b80958a52a27ba53
clux <analsandblaster@gmail.com>
2013-01-05 00:46:50 +0000

    fully moved over to x64 now (finally)

--- getSublimeUrl.js
+++ getSublimeUrl.js
@@ -1,6 +1,6 @@
 var Zombie = require('zombie');
 
 (new Zombie()).visit("http://www.sublimetext.com/2" , function (e, browser, status) {
-  var code = "document.querySelector('#dl_linux_32 a').href.replace(/ /g, '%20');";
+  var code = "document.querySelector('#dl_linux_64 a').href.replace(/ /g, '%20');";
   console.log(browser.evaluate(code));
 });
