483d86f8908dd1956f808a99155a1488f9d1d34f
clux <analsandblaster@gmail.com>
2013-01-22 23:56:05 +0000

    remove

--- getSublimeUrl.js
+++
@@ -1,6 +0,0 @@
-var Zombie = require('zombie');
-
-(new Zombie()).visit("http://www.sublimetext.com/2" , function (e, browser, status) {
-  var code = "document.querySelector('#dl_linux_64 a').href.replace(/ /g, '%20');";
-  console.log(browser.evaluate(code));
-});
