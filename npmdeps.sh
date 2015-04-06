82796ebbaf3a9cde1c1f642c680bfe2ea5dc332e
clux <analsandblaster@gmail.com>
2015-04-07 00:23:13 +0100

    split up repos and npmdeps

---
+++ npmdeps.sh
@@ -0,0 +1,33 @@
+# only set prefix if we didn't install from source
+#npm config set prefix ~/npm
+#npm config set loglevel info
+
+globalModules=(
+  ansimd      # markdown viewer terminal
+  jshint      # static analysis
+  jsonlint    # analysis of json files
+  json        # curl jsonUrl | json key
+  nd          # nd `moduleName` for README.md in stdout
+  npm         # alwyas update this
+  pkginit     # for quick making a npm module shell (though bugged atm)
+  browserify  # npm style webapps
+  testling    # for browser tests
+  asciify     # CLI asciifier
+  ytdl        # streaming youtube downloader
+  symlink     # node module link helper
+  badgify     # readme badge generator
+  bower       # web components currently here..
+  # tests
+  nodeunit
+  tap
+  coveralls
+  jscoverage
+  vulcanize   # polymer concat
+)
+for i in ${globalModules[@]}
+do
+  echo $i
+  npm install -g $i
+done
+
+pkginit add default pkg.json
