9cda10e191e86e05bb32e84a317798e37e1a1741
clux <analsandblaster@gmail.com>
2015-10-16 22:12:48 +0100

    make llvm task smarter

--- desktop.sh
+++ desktop.sh
@@ -2,19 +2,20 @@
 set -xeuo pipefail
 touch ~/.bashrc
 cd $(dirname $0)
-#./tasks/apt
+./tasks/apt
 ./tasks/node
-#source ~/.bashrc # need node
-#[ -z "$TRAVIS" ] && ./tasks/ssh
-#./tasks/sublime
-#[ -z "$TRAVIS" ] && ./tasks/cpy
-#./tasks/shell
-#source ~/.bashrc # need local shell stuff for npm PATH (maybe put that in node task)
-#./tasks/npm
+source ~/.bashrc # need node
+[ -z "$TRAVIS" ] && ./tasks/ssh
+./tasks/sublime
+[ -z "$TRAVIS" ] && ./tasks/cpy
+./tasks/shell
+source ~/.bashrc # need local shell stuff for npm PATH (maybe put that in node task)
+[ -z "$TRAVIS" ] && ./tasks/npm
 ./tasks/pip
 ./tasks/clone
-#[ -z "$TRAVIS" ] && ./tasks/llvm 3 7 0
-#./tasks/cleanup
-#source ~/.bashrc
+[ -z "$TRAVIS" ] && ./tasks/llvm 3.7.0
+./tasks/cleanup
+[ -z "$TRAVIS" ] && ./tasks/system
+source ~/.bashrc
 bats test/*.test.bats
 echo "All done - 'source ~/.bashrc' or open a new shell"
