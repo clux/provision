717dcf25cc31658f937fd5f991083193772378ae
clux <analsandblaster@gmail.com>
2015-10-17 14:37:50 +0100

    wtf travis

--- desktop.sh
+++ desktop.sh
@@ -2,15 +2,15 @@
 set -xeuo pipefail
 touch ~/.bashrc
 cd $(dirname $0)
-./tasks/apt
+#./tasks/apt
 ./tasks/node
 source ~/.bashrc # need node in other tasks
 [ -z "$TRAVIS" ] && ./tasks/ssh
-./tasks/sublime
-./tasks/cpy
-./tasks/npm
-./tasks/pip
-./tasks/clone
+#./tasks/sublime
+#./tasks/cpy
+#./tasks/npm
+#./tasks/pip
+#./tasks/clone
 [ -z "$TRAVIS" ] && ./tasks/llvm 3.7.0
 [ -z "$TRAVIS" ] && ./tasks/system
 ./tasks/bashrc
