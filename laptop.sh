2a6b0962e9c34af89d33c87973cd2229c3fd9050
clux <analsandblaster@gmail.com>
2015-11-08 18:14:12 +0000

    reformat laptop script

--- laptop.sh
+++ laptop.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -ex
-# This assumes you have setup `adduser clux sudo` as root user first
+# Run this with sudo before any tasks. After having run `adduser clux sudo`.
 
-# bunch of system stuff that needs sudo
+# package configuration and drivers
 sed -i.bak 's/jessie main/jessie main contrib non-free/g' /etc/apt/sources.list
 apt-get update
 apt-get install -y firmware-iwlwifi
-# GRUB is really slow to render on a high DPI screen - just keep it ugly
-sed -i.bak 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/' /etc/default/grub
-update-grub
 
 # https://wiki.debian.org/NvidiaGraphicsDrivers#Debian_8_.22Jessie.22
 aptitude -r install linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') nvidia-kernel-dkms
 # NB: this worked by itself last time - screw optimus on laptop (too much of a hassle)
 
-echo "laptop pre-setup script complete"
-echo "reboot and continue as if desktop"
+# GRUB is really slow to render on a high DPI screen - just keep it ugly
+sed -i.bak 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/' /etc/default/grub
+update-grub
+
+echo "Boot and continue with tasks"
