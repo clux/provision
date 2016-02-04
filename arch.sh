72b65ce4d8419403697adf9617ab4b12efa91b5a
clux <analsandblaster@gmail.com>
2016-02-04 23:00:24 +0000

    arch notes

--- arch.sh
+++ arch.sh
@@ -7,15 +7,6 @@ set -xeo pipefail
 cd "$(dirname "$0")"
 sudo chown -R "$USER" /usr/local
 
-localectl set-locale LANG=en_US.UTF-8
-# may need mintlocale from aur to do system wide stuff - sublime is complaining..
-
-# AUR install chrome
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/google-chrome.tar.gz | tar xzvf
-cd google-chrome
-makepkg -s
-sudo pacman -U google-chrome-*.pkg.tar.xz
-
 ./tasks/pacman
 ./tasks/sublime
 ./tasks/node
--- archboot.sh
+++ archboot.sh
@@ -3,73 +3,73 @@
 echo "This is a pseudo-script"
 exit 1
 
+# temporaries
 loadkeys colemak
-dhcpcd # easy on corded - verify you have inet
+dhcpcd # corded
 
 # partitioning
 fdisk -l # check the output
-cfdisk /dev/sda
+cfdisk /dev/sda # clear
+# /dev/sda1 :: primary bootable (disk size - 3G)
+# /dev/sda2 :: extended 3G
+# └─/dev/sda5 :: logical 3G
+
+# filesystems
 mkfs.ext4 -E discard /dev/sda1 # disable journaling on SSDs
 mount /dev/sda1 /mnt
-
-mkswap /dev/sda5 # the sub partition of the extended one
+# mkfs above seem stho shift sda3 -> sda5 (the logical partition we created)
+mkswap /dev/sda5
 swapon /dev/sda5
 
+# create chroot and do the first configuration
 pacstrap /mnt base base-devel
+systemd-firstboot --root=/mnt \
+  --timezone=Europe/London \
+  --locale=en_GB.UTF-8 \
+  --locale-messages=en_GB.UTF-8 \
+  --hostname=kjttks
+
 arch-chroot /mnt /bin/bash
-
-edit /etc/locale.gen # uncomment en_GB.* + en_US.* (2x2 elements)
-locale-gen
-
-tzselect
-# can add export TZ="Europe/London" in `~/.profile`
-ln -s /usr/share/zoneinfo/Europo/London /etc/localtime
-
 hwclock --systohc --utc
-
 mkinitcpio -p linux
 
 # bootloader to SSD
-pacman -S grub-bios
+pacman -S grub
 grub-mkconfig -o /boot/grub/grub.cfg
-grub-install /dev/sda
+grub-install /dev/sda # will use i386 by default
 
-echo "kjttks" > /etc/hostname
+# enable dhcpcd on right interface (see ip link show)
+systemctl enable dhcpcd@enp5s0
 
-# find interface in /sys/class/net (looks like et-- something)
-systemctl enable dhcpcd@interface.service
-
-passwd # set root pass
+# set root pass, exit chroot and reboot into roots account
+passwd
 exit # chroot
-umount /mnt
-swapoff -a
+reboot
 
-reboot now
-login # root
+# as root
+localectl set-keymap colemak
+timedatectl set-ntp true
 
-loadkeys colemak
 nano /etc/pacman.conf # uncomment multilib part (not testing)
 pacman -Syy
 pacman -Syu
 pacman -S sudo
-
 useradd -m -G games,rfkill,uucp,wheel -s /bin/bash clux
 passwd clux
-nano /etc/sudoers # uncomment %wheel ALL(ALL) ALL --- NOT NOPASSWD
+visudo # uncomment %wheel ALL(ALL) ALL --- NOT NOPASSWD
 
-pacman -S alsa-utils xorg-server ttf-dejavu ttf-liberation cinnamon nvidia lightdm lightdm-gtk-greeter
-# get nvidia-libgl version only with evdeb (libinput is for wayland)
+pacman -S nvidia # choose nvidia-libgl and evdev (which will pull xorg-server)
+reboot # blacklists noveu
 
-reboot
-login # clux
-
+# as root
+X # verify we can startx
+pacman -S alsa-utils cinnamon lightdm lightdm-gtk-greeter
+pacman -S ttf-dejavu ttf-liberation guake chromium wget
 
 nano /etc/lightdm/lightdm.conf # greeter-session=lightdm-gtk-greeter
 
-systemctl enable lightdm.service
-systemctl start lightdm
-pacman -S guake firefox wget
+reboot
 
-# ctlr-alt-F7 and log in to cinnamon session
-# change keyboard layout
-# alt-f2 start guake - not shortcutted
+# as clux
+localectl set-x11-keymap us pc104 colemak # greeter keyb
+sudo systemctl enable lightdm --now
