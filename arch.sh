d95f64337495986b2138cb8115d963d8355de732
clux <analsandblaster@gmail.com>
2016-02-07 08:49:21 +0000

    realizing some sites still need flash :/

--- arch.sh
+++ arch.sh
@@ -7,6 +7,12 @@ set -xeo pipefail
 cd "$(dirname "$0")"
 sudo chown -R "$USER" /usr/local
 
+# Need a chromey version with flash player still...
+wget -qO- https://aur.archlinux.org/cgit/aur.git/snapshot/google-chrome.tar.gz | tar xzv
+cd google-chrome
+makepkg -s
+pacman -U google-chrome-*.pkg.tar.xz
+
 ./tasks/pacman
 ./tasks/sublime
 ./tasks/node
