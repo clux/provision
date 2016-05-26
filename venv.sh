0f1f8e7ab6471c968425e947ee66d7bcd251a95a
clux <analsandblaster@gmail.com>
2016-05-27 00:33:34 +0100

    boring testing strategy - closes #26

--- venv.sh
+++ venv.sh
@@ -8,6 +8,7 @@ if [ ! -d venv ]; then
   virtualenv2 -p $(which python2) venv
 fi
 source venv/bin/activate
+
+export ANSIBLE_NOCOWS=1
 echo "Entering virtual env $VIRTUAL_ENV"
 pip install -r requirements.txt
-export ANSIBLE_NOCOWS=1
