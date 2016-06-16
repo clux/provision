f0e4f0b97fdf6e8f8a6b443c82cf1e30a5e11806
clux <analsandblaster@gmail.com>
2016-06-16 20:39:02 +0100

    some more sanity checks on ssh key

--- genprov.sh
+++ genprov.sh
@@ -3,6 +3,11 @@ set -ex
 
 disk=${1-/dev/sdd}
 
+if [ ! -f ~/.ssh/main_id ]; then
+  echo "You need to have an authorized SSH key to export"
+  exit 1
+fi
+
 echo "WARNING: This will completely overwrite ${disk}"
 read -rp "Press any key to continue"
 
@@ -15,7 +20,11 @@ sudo mkfs.ext4 "${disk}1"
 mkdir -p stick
 sudo mount "${disk}1" stick
 sudo chown "$USER:$USER" stick
-echo "Encrypting ssh key"
-gpg -c ~/.ssh/main_id
+if [ ! -f ~/.ssh/main_id.gpg ]; then
+  echo "Encrypting ssh key"
+  gpg -c ~/.ssh/main_id
+fi
 cp archboot/* stick/
 cp ~/.ssh/main_id.{pub,gpg} stick/
+sudo umount "${disk}1" || true
+rm -f stick
