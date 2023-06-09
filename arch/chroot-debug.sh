a7eaae5d1f239a85ad4287c777915e9fffbefcd8
clux <sszynrae@gmail.com>
2023-06-09 19:53:18 +0100

    notes to self on chrooting
    Signed-off-by: clux <sszynrae@gmail.com>

--- arch/chroot-debug.sh
+++ arch/chroot-debug.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -exuo pipefail
 
+iwctl
+station wlan0 show
+station wlan0 scan
+station wlan0 connect TACOCAT
+exit
 # boot with an arch img and fiddle with the chroot if you have broken things:
 
 loadkeys colemak
-cryptsetup luksOpen /dev/nvme0n1p2 luks
+cryptsetup luksOpen /dev/nvme0n1p2 cryptlvm
 
 mount /dev/mapper/vg0-root /mnt
 swapon /dev/mapper/vg0-swap
 mount /dev/nvme0n1p1 /mnt/boot
 arch-chroot /mnt /bin/bash
 
-# fiddle, then
+# fiddle, then (probably mkinitcpio if something was broken)
 exit
 
 # Cleanup
